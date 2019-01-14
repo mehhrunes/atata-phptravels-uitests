5967dc90cfb3297b44ce2301eb2ae6a4aafe9e5f
mehhrunes <roma7155@gmail.com>
2019-01-14 16:29:48 +0200

    changed copy method

--- build.ps1
+++ build.ps1
@@ -90,7 +90,8 @@ Function CopyBuildArtifacts()
     #           which will get items (Get-ChildItem) and will copy them (Copy-Item) to the target folder
 
     if(Test-Path $SourceFolder){
-        Copy-Item -Force -Recurse -Verbose $SourceFolder -Destination $DestinationFolder
+        # Copy-Item -Force -Recurse -Verbose $SourceFolder -Destination $DestinationFolder
+        Get-ChildItem $SourceFolder | Copy-Item $DestinationFolder
     }
     if($Error){
         Throw "An error occured while copying build artifacts."
